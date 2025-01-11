0352ac8bb888a32549f2126eb3e7fb9b46a19d87
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-01-10 18:30:31 -0800

    implemented working solution.

---
+++ ExploreGetRssFeed/Models/FeedItem.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace ExploreGetRssFeed.Models
+{
+    public class FeedItem
+    {
+        public int? Id { get; set; }
+        [Display(Name = "Title")]
+        public string Title { get; set; } = "Untitled";
+        [Display(Name = "Link")]
+        public string Link { get; set; } = string.Empty;
+        [Display(Name = "Creator")]
+        public string Creator { get; set; } = "Unlisted";
+        [Display(Name = "Publish Date")]
+        public DateTime PubDate { get; set; }
+        [Display(Name = "Description")]
+        public string Description { get; set; } = "<p>No description</p>";
+        [Display(Name = "Content")]
+        public string Content { get; set; } = "None";
+
+        public MarkupString DescriptionAsHtml => new MarkupString(Description);
+        public MarkupString LinkAsHtml => new MarkupString($"<a href=\"{Link}\">{Link}</a>");
+
+        public static FeedItem Create(string title, string link, string pubDate, string description)
+        {
+            string cleanTitle = title.Trim();
+            string cleanLink = CleanLink(link);
+            DateTime cleanPubDate = string.IsNullOrWhiteSpace(pubDate) ? DateTime.Now : DateTime.Parse(pubDate);
+            string cleanDescription = CleanDescription(description);
+
+            return new FeedItem()
+            {
+                Title = cleanTitle,
+                Link = cleanLink,
+                PubDate = cleanPubDate,
+                Description = cleanDescription,
+            };
+        }
+
+        public static FeedItem Create(string title, string link, string description)
+        {
+            string cleanTitle = title.Trim();
+            string cleanLink = CleanLink(link);
+            string cleanDescription = CleanDescription(description);
+
+            return new FeedItem()
+            {
+                Title = cleanTitle,
+                Link = cleanLink,
+                Description = cleanDescription,
+            };
+        }
+
+        // Get the first paragraph content only.
+        public static string CleanDescription(string description)
+        {
+            string defaultDescription = "<p>No description</p>";
+
+            // 3 cases:
+            // 1. description is empty. Return the default description.
+            // 2. description has content but no paragraph tags: Return the trimmed description.
+            // 3. description has content AND paragraph tags: Capture the content between the first
+            // pair of paragraph tags, then trim and return it
+
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                return defaultDescription;
+            }
+
+            string pattern = @"<p>(.*?)</p>";
+            var match = Regex.Match(description, pattern, RegexOptions.IgnoreCase);
+            return match.Success ? 
+                match.Groups[1].Value.Trim() : 
+                description.Trim();
+        }
+
+        // Get only the link with the protocol, host, and path.
+        public static string CleanLink(string link)
+        {
+            if (string.IsNullOrWhiteSpace(link))
+            {
+                return string.Empty;
+            }
+
+            int hIndex = link.Contains("http") ? link.IndexOf("http") : 0;
+            int endLinkIndex = link.Contains("</link>") ? link.IndexOf("</link>") : 0;
+            string result = link.Substring(hIndex, link.Length - endLinkIndex);
+            return result;
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# RSS Feed Getter
+
+An experimental project that will display RSS Feed data in a dynamic web page.
+
+## Overview
+
+This is a simple experimental Web App to get the RSS feed from a website and display it in a simple HTML page.
+
+## Features
+
+- Built using ASP.NET Blazor 8.0
+
+## Requirements
+
+- RSS Feed URLs must be known, valid, and configured in `appsettings.json`
+- DotNET 8.0
+- An internet connection
+
+## Future
+
+As time permits, I might add new features to this project. Some top-of-mind ideas:
+
+- Dynamically adding RSS Feed URLs based on appsettings or user input.
+- Updating the Style, perhaps to a theme or two.
+- Better processing of feed data to ensure content is displayed correctly. For example, some RSS Feed elements contain HTML tags, others do not, so I assume there will be still others that contain all sorts of unexpected delimiters, character sets and encodings, etc.
+ 
