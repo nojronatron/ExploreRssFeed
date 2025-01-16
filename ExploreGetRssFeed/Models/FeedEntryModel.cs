60123f83d7e7d8b1568aa6ed3a1fb781ebaf77be
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-01-15 17:37:48 -0800

    Load test feeds IConfiguration, update component names

---
+++ ExploreGetRssFeed/Models/FeedEntryModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExploreGetRssFeed.Models
+{
+    public class FeedEntryModel
+    {
+        [Required(ErrorMessage = "Make up a short title for this RSS Feed.")]
+        [StringLength(maximumLength: 30, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Required]
+        [Url(ErrorMessage = "The url does not match an expected format.")]
+        public string WebAddress { get; set; }
+
+        public string BaseUrl = "/rssfeeds/";
+
+        public string? PathUrl
+        {
+            get
+            {
+                return string.Concat(BaseUrl, Title.Replace(" ", ""));
+            }
+        }
+
+        public string ToRow()
+        {
+            return $"{Title}: {WebAddress}";
+        }
+    }
+}
